e0adf54536dc6d1baf5e005cacd379d71718ce02
evgeny <evg299792458@yandex.ru>
2020-02-14 09:39:28 +0300

    PHOTOMARK-28, PHOTOMARK-29

--- apps/rest-app/src/main/java/ru/codeforensics/photomark/restapp/security/UserSessionDetailsService.java
+++ apps/rest-app/src/main/java/ru/codeforensics/photomark/restapp/security/UserSessionDetailsService.java
@@ -2,6 +2,7 @@ package ru.codeforensics.photomark.restapp.security;
 
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,6 +14,9 @@ import ru.codeforensics.photomark.model.repo.UserSessionRepo;
 @Service
 public class UserSessionDetailsService implements UserDetailsService {
 
+  @Value("${app.superadmin.xapikey}")
+  private String xApiKeySuperAdmin;
+
   @Autowired
   private UserSessionRepo userSessionRepo;
 
@@ -20,6 +24,10 @@ public class UserSessionDetailsService implements UserDetailsService {
   public UserDetails loadUserByUsername(String xApiKey) throws UsernameNotFoundException {
     if (StringUtils.isEmpty(xApiKey)) {
       throw new UsernameNotFoundException("API key is empty");
+    }
+
+    if (xApiKeySuperAdmin.equals(xApiKey)) {
+      return new SuperAdminUserDetails();
     }
 
     Optional<UserSession> userSessionOptional = userSessionRepo.findByToken(xApiKey);
---
+++ transfer/src/main/java/ru/codeforensics/photomark/transfer/UserTransfer.java
@@ -0,0 +1,18 @@
+package ru.codeforensics.photomark.transfer;
+
+import java.util.HashSet;
+import java.util.Set;
+import lombok.Data;
+import ru.codeforensics.photomark.transfer.enums.UserAuthority;
+
+@Data
+public class UserTransfer {
+
+  private Long id;
+
+  private String email;
+
+  private String password;
+
+  private Set<UserAuthority> authorities = new HashSet<>();
+}
